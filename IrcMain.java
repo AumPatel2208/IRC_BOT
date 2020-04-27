7442329620cb293dc78ec09e5c02b6690c2a2f53
Aum <imaum2208@gmail.com>
2020-04-27 22:37:20 +0100

    Update names list on state change

--- IrcMain.java
+++ IrcMain.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +20,11 @@ class IrcMain {
     private static final String CMD_HELLO = "hello";
     private static final String CMD_HACK = "hack";
     private static final String CMD_ATTACK = "attack";
-    // private static final String CHANNEL = "#thebois";
-    private static final String CHANNEL = "#Goethe";
+    private static final String CMD_TIME = "time";
+    private static final String CMD_TIME_ZONES = "zones";
+
+    private static final String CHANNEL = "#thebois";
+    // private static final String CHANNEL = "#Goethe";
 
     public static void main(String[] args) throws IOException {
         Scanner console = new Scanner(System.in);
@@ -35,6 +42,7 @@ class IrcMain {
         // write("JOIN", "#thebois");
         write("JOIN", CHANNEL);
         boolean active = false;
+
         String[] names = new String[0]; // names
 
         while (in.hasNext()) {
@@ -42,11 +50,50 @@ class IrcMain {
 
             System.out.println("<<< " + serverMessage);
 
-            if (serverMessage.contains("353")) {// Names List
+            // Names List
+            if (serverMessage.contains("353")) {
                 names = serverMessage.split(":")[2].trim().split(" "); // got the names
                 // for (String name : names) {
                 // System.out.println(name);
                 // }
+            }
+
+            // Add new person that joins into the list
+            if (serverMessage.contains("JOIN")) {
+                String[] tempNames = new String[1 + names.length];
+
+                for (int i = 0; i < names.length; i++) {
+                    tempNames[i] = names[i];
+                }
+                tempNames[tempNames.length - 1] = serverMessage.split(":")[1].split("!")[0];
+                names = tempNames;
+            }
+
+            // If person is kicked, update names list
+            if (serverMessage.contains("KICK")) {
+
+                String tempString = "";
+
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(serverMessage.split(CHANNEL)[1].trim().split(" ")[0])) {
+                        names[i] = "";
+                    }
+                    tempString += names[i] + " ";
+                }
+                names = tempString.split(" ");
+            }
+
+            // remove person that 'parts' from the list
+            if (serverMessage.contains("PART")) {
+                String tempString = "";
+
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i].equals(serverMessage.split(":")[1].split("!")[0])) {
+                        names[i] = "";
+                    }
+                    tempString += names[i] + " ";
+                }
+                names = tempString.split(" ");
             }
 
             if (serverMessage.contains("366")) // End of Names List
@@ -92,6 +139,31 @@ class IrcMain {
                             writeMessage("Please select a person to attack (type 'bobbot attack <name>' ): ");
                         } else if (messageSplit[1].toLowerCase().equals(CMD_ATTACK)) {
                             // What to do when attacking
+                            if (messageSplit.length >= 3) {
+                                writeMessage("I banish you from the server " + messageSplit[2]);
+                            }
+                        } else if (messageSplit[1].toLowerCase().equals(CMD_TIME)) { // SCRAP THIS COMMAND
+
+                            URL url = new URL("http://worldtimeapi.org/api/timezone/europe/london.txt");
+                            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                            con.setRequestMethod("GET");
+                            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                            String inputLine;
+                            StringBuffer content = new StringBuffer();
+                            while ((inputLine = in.readLine()) != null) {
+                                content.append(inputLine);
+                            }
+                            con.disconnect();
+
+                            in.close();
+                            System.out.println(content);
+                            String output = content.toString();
+                            writeMessage(output);
+                            if (messageSplit.length >= 3) {
+                                if (messageSplit[2].toLowerCase().equals(CMD_TIME_ZONES)) {
+                                    // Make API request to write out all of the time zones
+                                }
+                            }
                         } else {
                             // Write what the person says
                             writeMessage(messageSplit[1]);
